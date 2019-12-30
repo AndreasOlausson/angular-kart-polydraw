674be1f5847462b2869e9e5cdfac011aec3485ed
Andreas <andreas@codepoets.se>
2019-12-30 08:52:32 +0100

    Create enums.ts

---
+++ src/app/map/enums.ts
@@ -0,0 +1,20 @@
+export enum DrawMode {
+    Off = 0,
+    Add = 1,
+    Edit = 2,
+    Subtract = 3,
+    LoadPredefined = 4
+}
+export enum MarkerPlacement {
+    Center = 0,
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4,
+    NorthEast = 5,
+    NorthWest = 6,
+    SouthEast = 7,
+    SouthWest = 8
+}
+
+
