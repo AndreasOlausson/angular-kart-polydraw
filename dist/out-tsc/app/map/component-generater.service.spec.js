ef71ac778d08f71a11ae8edd2c39ee9b5bc710af
Truls Garder <trg@eiendomsverdi.no>
2020-01-20 12:53:58 +0100

    added dist for npm-testing

---
+++ dist/out-tsc/app/map/component-generater.service.spec.js
@@ -0,0 +1,14 @@
+/* import { TestBed } from '@angular/core/testing';
+
+import { ComponentGeneraterService } from './component-generater.service';
+
+describe('ComponentGeneraterService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ComponentGeneraterService = TestBed.get(ComponentGeneraterService);
+    expect(service).toBeTruthy();
+  });
+});
+ */ 
+//# sourceMappingURL=component-generater.service.spec.js.map
