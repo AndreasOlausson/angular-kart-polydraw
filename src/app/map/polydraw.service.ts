16980e2b3286e8c988c30b8136b6e3da1c82ab73
Truls Garder <trg@eiendomsverdi.no>
2020-01-20 15:49:50 +0100

    Testing without injectable

--- src/app/map/polydraw.service.ts
+++ src/app/map/polydraw.service.ts
@@ -14,9 +14,9 @@ import { Compass, PolyDrawUtil } from "./utils";
 import { MarkerPlacement } from "./enums";
 import { LeafletHelperService } from "./leaflet-helper.service";
 
-/* @Injectable({
+@Injectable({
   providedIn: "root"
-}) */
+})
 //Rename - PolyDrawService
 export class PolyDrawService {
   //DrawModes, determine UI buttons etc...
