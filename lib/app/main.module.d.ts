dfe09e2034db0b3da11f73f05f81e63081716f19
Truls Garder <trg@eiendomsverdi.no>
2020-01-20 16:16:56 +0100

    Testing without injectable

--- lib/app/main.module.d.ts
+++ lib/app/main.module.d.ts
@@ -1,3 +1,4 @@
+export { AlterPolygonComponent } from './map/popups/alter-polygon/alter-polygon.component';
 export { PolygonInformationService } from "./map/polygon-information.service";
 export { MapStateService } from "./map/map-state.service";
 export { PolyDrawService } from "./map/polydraw.service";
--- src/app/main.module.ts
+++ src/app/main.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AlterPolygonComponent } from './map/popups/alter-polygon/alter-polygon.component';
+export  { AlterPolygonComponent } from './map/popups/alter-polygon/alter-polygon.component';
 export {PolygonInformationService} from "./map/polygon-information.service"
 export {MapStateService} from "./map/map-state.service"
 export {PolyDrawService} from "./map/polydraw.service"
+
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ 
