242b7dbf93e2ed324b059cc88ad720d4ad57c081
Truls Garder <trg@eiendomsverdi.no>
2020-03-09 15:54:27 +0100

    test

--- dist/my-lib/public-api.d.ts
+++ dist/my-lib/public-api.d.ts
@@ -1,8 +1,8 @@
-export * from './lib/polydraw.service';
+export { PolyDrawService } from './lib/polydraw.service';
 export * from './lib/popups/alter-polygon/alter-polygon.component';
-export * from './lib/map-state.service';
+export { PolyStateService } from './lib/map-state.service';
 export * from './lib/component-generater.service';
-export * from './lib/polygon-information.service';
+export { PolygonInformationService } from './lib/polygon-information.service';
 export * from './lib/my-lib.module';
 export * from './lib/enums';
 export * from './lib/polygon-helpers';
--- projects/my-lib/src/public-api.ts
+++ projects/my-lib/src/public-api.ts
@@ -1,12 +1,13 @@
+
 /*
  * Public API Surface of my-lib
  */
 
-export * from './lib/polydraw.service';
+export {PolyDrawService} from './lib/polydraw.service';
 export * from './lib/popups/alter-polygon/alter-polygon.component';
-export * from './lib/map-state.service';
+export {PolyStateService} from './lib/map-state.service';
 export * from './lib/component-generater.service';
-export * from './lib/polygon-information.service';
+export {PolygonInformationService} from './lib/polygon-information.service';
 export * from './lib/my-lib.module';
 export * from './lib/enums';
 export * from './lib/polygon-helpers';
