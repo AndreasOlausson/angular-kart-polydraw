686a9c40bf9ad6e8b85908055aa825b6820e91a2
andreasolausson <andreas@codepoets.se>
2020-04-03 21:36:36 +0200

    added another star polyon, changed map type

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -61,7 +61,13 @@ export class MapComponent {
         // this.mapState.updateMapState(this.map);
 
 
-        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        // L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        //     maxZoom: 20,
+        //     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        // }as any).addTo(this.map);
+        // this.mapState.updateMapState(this.map);
+
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
             maxZoom: 20,
             attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         }as any).addTo(this.map);
